fc1908fa89448bfd232da57570b9f4b520d86c85
85468 <854687148@qq.com>
2021-10-17 23:59:12 +0800

    choList=[]
    def exist(a):
        if '4' in a and '1' in a and '5' in a:
            return 5
        elif '4' in a and '1' in a :
            return 3
        elif '1' in a:
            return 1
        elif '4' in a:
            return 4

--- Dazuoye_1/Dazuoye.py
+++ Dazuoye_1/Dazuoye.py
@@ -5,3 +5,15 @@ Date started:10|16
 from operator import itemgetter
 
 import csv
+
+choList=[]
+
+def exist(a):
+    if '4' in a and '1' in a and '5' in a:
+        return 5
+    elif '4' in a and '1' in a :
+        return 3
+    elif '1' in a:
+        return 1
+    elif '4' in a:
+        return 4
